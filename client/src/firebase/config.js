480de2c57ebe3b4c37c7b29257726314194af2a9
Kushagra Bajpei <kushagrabajpeimbic@gmail.com>
2025-10-19 20:09:32 +0530

    send

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -1,28 +1,4 @@
 // src/firebase/config.js
-// import { initializeApp } from "firebase/app";
-// import { getAuth, GoogleAuthProvider } from "firebase/auth";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAv76IgMuM80gOmrGMedq7VVDNQHSLHMxs",
-//   authDomain: "health-n-hacks.firebaseapp.com",
-//   projectId: "health-n-hacks",
-//   storageBucket: "health-n-hacks.firebasestorage.app",
-//   messagingSenderId: "1002610149923",
-//   appId: "1:1002610149923:web:c25043ff586ef3db117f96",
-//   measurementId: "G-4V7GD2ZJWD"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-
-// // Auth + Google Provider
-// export const auth = getAuth(app);
-// export const googleProvider = new GoogleAuthProvider();
-
-// export default app;
-
-
-// src/firebase/config.js
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
